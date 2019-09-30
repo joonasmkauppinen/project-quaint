982367cc3c9c212cbbe09c5d497f8e95c7581fba
Joonas Kauppinen <joonasm.kauppinen@gmail.com>
2019-09-30 13:11:40 +0300

    Add: dark styles to map

--- app/src/main/java/com/newgat/quaint/MapFragment.kt
+++ app/src/main/java/com/newgat/quaint/MapFragment.kt
@@ -1,9 +1,11 @@
 package com.newgat.quaint
 
 import android.content.Context
+import android.content.res.Resources
 import android.location.Location
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -11,12 +13,16 @@ import android.view.ViewGroup
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MapStyleOptions
 import com.google.android.gms.maps.model.MarkerOptions
 import kotlinx.android.synthetic.main.fragment_map.view.*
 
 
 class MapFragment : Fragment(), OnMapReadyCallback, View.OnClickListener {
+
+    private val TAG = "MapFragment"
 
     private var listener: OnFragmentInteractionListener? = null
     private lateinit var  fusedLocationProviderClient: FusedLocationProviderClient
@@ -39,14 +45,23 @@ class MapFragment : Fragment(), OnMapReadyCallback, View.OnClickListener {
         mapFragment.getMapAsync(this)
     }
 
-    override fun onMapReady(googleMap: GoogleMap) {
-        map = googleMap
+    override fun onMapReady(map: GoogleMap) {
         fusedLocationProviderClient.lastLocation.addOnSuccessListener { location: Location ->
             val currentLocation = LatLng(location.latitude, location.longitude)
+            try {
+                 val success = map.setMapStyle(
+                    MapStyleOptions.loadRawResourceStyle(context, R.raw.maps_style_json)
+                 )
+                if (!success) Log.d(TAG, "Style parsing failed.")
+            } catch (e: Resources.NotFoundException) {
+                Log.d(TAG, "Can't find style. Error: $e")
+            }
+
             map.addMarker(
                 MarkerOptions()
                     .position(currentLocation)
                     .title("Your last known location")
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW))
             )
             map.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 15f))
         }
