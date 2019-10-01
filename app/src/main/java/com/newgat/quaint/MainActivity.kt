75f620a1b36e62cf06fd623a5110fc9cd21fd70b
Alex Mäkinen <alex.makinen@metropolia.fi>
2019-10-01 15:11:28 +0300

    eilisen sähellykset

--- app/src/main/java/com/newgat/quaint/MainActivity.kt
+++ app/src/main/java/com/newgat/quaint/MainActivity.kt
@@ -11,6 +11,8 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.FragmentTransaction
 import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.Geofence
+import com.google.android.gms.location.GeofencingClient
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -18,22 +20,34 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.common.base.Objects
+import com.google.common.base.Strings
 
 class MainActivity : AppCompatActivity(), MapFragment.OnFragmentInteractionListener {
 
+    lateinit var geofencingClient: GeofencingClient
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
     companion object {
-        const val APP_PERMISSIONS_ACCESS_FINE_LOCATION = 1
+        const val APP_PERMISSIONS_ACCESS_BACKGROUND_LOCATION = 1
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-            != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this,
-                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-                APP_PERMISSIONS_ACCESS_FINE_LOCATION)
+        geofencingClient = LocationServices.getGeofencingClient(this)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_BACKGROUND_LOCATION),
+                APP_PERMISSIONS_ACCESS_BACKGROUND_LOCATION
+            )
         }
     }
 
@@ -47,5 +61,35 @@ class MainActivity : AppCompatActivity(), MapFragment.OnFragmentInteractionListe
 
     override fun onFragmentInteraction() {
         onBackPressed()
+    }
+
+    fun geoFenceTest(v: View) {
+        val geofences = mutableListOf<Geofence>()
+        fusedLocationClient.lastLocation.addOnSuccessListener { location: Location ->
+        geofences.add(
+            Geofence.Builder()
+                // Set the request ID of the geofence. This is a string to identify this
+                // geofence.
+                .setRequestId("test")
+
+                // Set the circular region of this geofence.
+                .setCircularRegion(
+                    location.latitude,
+                    location.longitude,
+                    50f
+                )
+
+                // Set the expiration duration of the geofence. This geofence gets automatically
+                // removed after this period of time.
+                .setExpirationDuration(10000)
+
+                // Set the transition types of interest. Alerts are only generated for these
+                // transition. We track entry and exit transitions in this sample.
+                .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER or Geofence.GEOFENCE_TRANSITION_EXIT)
+
+                // Create the geofence.
+                .build()
+        )
+    }
     }
 }
