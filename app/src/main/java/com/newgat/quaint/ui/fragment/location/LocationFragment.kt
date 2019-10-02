9299a6a8cddedb976032a397042b045bc7ba49a8
Joonas Kauppinen <joonasm.kauppinen@gmail.com>
2019-10-02 14:49:45 +0300

    Add: LocationFragment + ViewModel

---
+++ app/src/main/java/com/newgat/quaint/ui/fragment/location/LocationFragment.kt
@@ -0,0 +1,33 @@
+package com.newgat.quaint.ui.fragment.location
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.newgat.quaint.R
+
+class LocationFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = LocationFragment()
+    }
+
+    private lateinit var viewModel: LocationViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.location_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(LocationViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
