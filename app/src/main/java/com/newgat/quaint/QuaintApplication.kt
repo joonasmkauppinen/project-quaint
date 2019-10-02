c8f9bb12e3c984ff14a4256ca2ce1e4e839c9963
Joonas Kauppinen <joonasm.kauppinen@gmail.com>
2019-10-02 13:43:59 +0300

    Add: dependency injection

---
+++ app/src/main/java/com/newgat/quaint/QuaintApplication.kt
@@ -0,0 +1,22 @@
+package com.newgat.quaint
+
+import android.app.Application
+import com.newgat.quaint.data.db.QuaintDatabse
+import com.newgat.quaint.data.repository.QuaintRepository
+import com.newgat.quaint.data.repository.QuaintRepositoryImpl
+import org.kodein.di.Kodein
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.x.androidXModule
+import org.kodein.di.generic.bind
+import org.kodein.di.generic.instance
+import org.kodein.di.generic.singleton
+
+class QuaintApplication : Application(), KodeinAware {
+    override val kodein = Kodein.lazy {
+        import(androidXModule(this@QuaintApplication))
+
+        bind() from singleton { QuaintDatabse(instance()) }
+        bind() from singleton { instance<QuaintDatabse>().locationsDao() }
+        bind<QuaintRepository>() with singleton { QuaintRepositoryImpl(instance()) }
+    }
+}
