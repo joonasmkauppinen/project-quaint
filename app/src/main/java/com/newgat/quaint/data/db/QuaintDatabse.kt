9169bb303ed7bc97ed4874b38b558307ee34878b
Joonas Kauppinen <joonasm.kauppinen@gmail.com>
2019-10-02 11:41:53 +0300

    Add: room db

---
+++ app/src/main/java/com/newgat/quaint/data/db/QuaintDatabse.kt
@@ -0,0 +1,31 @@
+package com.newgat.quaint.data.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.newgat.quaint.data.db.entity.LocationEntry
+
+@Database(
+    entities = [LocationEntry::class],
+    version = 1
+)
+abstract class QuaintDatabse : RoomDatabase() {
+    abstract fun locationsDao(): LocationsDao
+
+    // Create database singleton
+    companion object {
+        // @Volatile inorder for all threads to have immediate access to property
+        @Volatile private var instance: QuaintDatabse? = null
+        private val LOCK = Any()
+
+        operator fun invoke(context: Context) = instance ?: synchronized(LOCK) {
+            instance ?: buildDatabase(context).also { instance = it }
+        }
+
+        private fun buildDatabase(context: Context) =
+            Room.databaseBuilder(context.applicationContext,
+                QuaintDatabse::class.java, "quaint.db")
+                .build()
+    }
+}
