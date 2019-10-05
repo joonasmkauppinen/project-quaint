a0528468949c87f3709de545ed19e25eca5275f0
Joonas Kauppinen <joonasm.kauppinen@gmail.com>
2019-10-05 13:16:45 +0300

    Add: bottom sheet modal (dialog)

---
+++ app/src/main/java/com/newgat/quaint/ui/fragment/bottomsheetmodal/BottomSheetModalViewModel.kt
@@ -0,0 +1,7 @@
+package com.newgat.quaint.ui.fragment.bottomsheetmodal
+
+import androidx.lifecycle.ViewModel
+
+class BottomSheetModalViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
