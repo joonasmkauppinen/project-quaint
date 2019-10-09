6a250b6113d68bc4374e5046b46a37d1c2c6f955
Joonas Kauppinen <joonasm.kauppinen@gmail.com>
2019-10-09 20:38:46 +0300

    Update: dependency injection

--- app/src/main/java/com/newgat/quaint/QuaintApplication.kt
+++ app/src/main/java/com/newgat/quaint/QuaintApplication.kt
@@ -25,8 +25,10 @@ class QuaintApplication : Application(), KodeinAware {
         bind() from singleton { instance<QuaintDatabase>().locationsDao() }
         bind<ConnectivityInterceptor>() with singleton { ConnectivityInterceptorImpl(instance()) }
         bind() from singleton { GooglePlacesApiService(instance()) }
+        bind() from singleton { GoogleGeocodingService(instance()) }
         bind<GooglePlacesDataSource>() with singleton { GooglePlacesDataSourceImpl(instance()) }
-        bind<QuaintRepository>() with singleton { QuaintRepositoryImpl(instance(), instance()) }
+        bind<GoogleGeocodingDataSource>() with singleton { GoogleGeocodingDataSourceImpl(instance()) }
+        bind<QuaintRepository>() with singleton { QuaintRepositoryImpl(instance(), instance(), instance()) }
         bind() from provider { LocationsSectionViewModelFactory(instance()) }
         bind() from provider { NewLocationFormViewModelFactory(instance()) }
         bind() from provider { AddressSearchViewModelFactory(instance()) }
