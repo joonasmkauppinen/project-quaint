f0ff237accb3036ddf3d95d4086c8bd204ad446a
Joonas Kauppinen <joonasm.kauppinen@gmail.com>
2019-10-09 20:36:48 +0300

    Add: google geocoding service

---
+++ app/src/main/java/com/newgat/quaint/data/network/GoogleGeocodingService.kt
@@ -0,0 +1,52 @@
+package com.newgat.quaint.data.network
+
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import com.newgat.quaint.GCP_API_KEY
+import com.newgat.quaint.data.network.response.GoogleGeocodingResponse
+import kotlinx.coroutines.Deferred
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface GoogleGeocodingService {
+
+    @GET("json?")
+    fun getGeocodingForAddress(
+        @Query("place_id") placeId: String
+    ): Deferred<GoogleGeocodingResponse>
+
+    companion object {
+        operator fun invoke(
+            connectivityInterceptor: ConnectivityInterceptor
+        ): GoogleGeocodingService {
+            // Intercept the request to add api key into the request
+            val requestInterceptor = Interceptor { chain ->
+                val url = chain.request()
+                    .url()
+                    .newBuilder()
+                    .addQueryParameter("key", GCP_API_KEY)
+                    .build()
+                val request = chain.request()
+                    .newBuilder()
+                    .url(url)
+                    .build()
+                return@Interceptor chain.proceed(request)
+            }
+            val okHttpClient = OkHttpClient.Builder()
+                .addInterceptor(requestInterceptor)
+                .addInterceptor(connectivityInterceptor)
+                .build()
+            return Retrofit.Builder()
+                .client(okHttpClient)
+                .baseUrl("https://maps.googleapis.com/maps/api/geocode/")
+                .addCallAdapterFactory(CoroutineCallAdapterFactory())
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(GoogleGeocodingService::class.java)
+        }
+    }
+
+}
--- app/src/main/java/com/newgat/quaint/data/network/response/GoogleGeocodingResponse.kt
+++ app/src/main/java/com/newgat/quaint/data/network/response/GoogleGeocodingResponse.kt
@@ -3,7 +3,7 @@ package com.newgat.quaint.data.network.response
 
 import com.newgat.quaint.data.db.entity.Result
 
-data class GoogleGeocodeResponse(
+data class GoogleGeocodingResponse(
     val results: List<Result>,
     val status: String
 )
