fbc411a34886bd8bfef1f5809a973217d3e9b332
Alex Mäkinen <alex.makinen@metropolia.fi>
2019-10-10 23:56:00 +0300

    added broadcast receiver and geofencetransitionsjobintentservice

--- app/src/main/java/com/newgat/quaint/data/geofencing/GeofenceTransitionsJobIntentService.kt
+++ app/src/main/java/com/newgat/quaint/data/geofencing/GeofenceTransitionsJobIntentService.kt
@@ -41,7 +41,7 @@ class GeofenceTransitionsJobIntentService : JobIntentService() {
         val geofenceTransition = geofencingEvent.geofenceTransition
 
         // Test that the reported transition was of interest.
-        if (geofenceTransition == Geofence.GEOFENCE_TRANSITION_ENTER || geofenceTransition == Geofence.GEOFENCE_TRANSITION_EXIT) {
+        if (geofenceTransition == Geofence.GEOFENCE_TRANSITION_ENTER) {
 
             // Get the geofences that were triggered. A single event can trigger multiple geofences.
             val triggeringGeofences = geofencingEvent.triggeringGeofences
